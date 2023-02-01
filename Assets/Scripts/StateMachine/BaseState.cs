51c0f73a6748b3c1e8cbfed5e37278010438ebed
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-02-01 20:18:36 +0100

    Initial commit

---
+++ Assets/Scripts/StateMachine/BaseState.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CharacterController {
+    public abstract class BaseState : State {
+        
+        public CharacterController owner;
+        private CharacterController _character;
+
+        protected CharacterController Character => owner;
+    }
+}
---
+++ Assets/Scripts/StateMachine/JumpState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace CharacterController {
+    public class JumpState : BaseState {
+        private bool _falling = false;
+
+        private const string State = "JumpState";
+        public override void Enter() {
+            StateChange.stateUpdate?.Invoke(State);
+            _falling = false;
+            Character._jumpedOnce = true;
+            Character.airTime = 0;
+            Character._velocity.y = Character._jumpForce;
+        }
+
+        public override void Run() {
+
+            Character.AirControl();
+
+            Vector3 gravityMovement = Character._defaultGravity * Time.deltaTime * Vector3.down;
+
+            Character._velocity += gravityMovement;
+
+            Character.ApplyAirFriction();
+
+            if (!Character.HoldingJump || Character._velocity.y < float.Epsilon)
+                _falling = true;
+
+            if (_falling)
+                stateMachine.TransitionTo<AirState>();
+
+            if (WallRunState.Requirement(Character))
+                stateMachine.TransitionTo<WallRunState>();
+
+            if (Character.Grounded && Character._velocity.y < float.Epsilon)
+                stateMachine.TransitionTo<MoveState>();
+        }
+
+        public override void Exit() { }
+    }
+}
