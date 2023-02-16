0ea16492faf29d28e0c2bd8511460bcce3029a9e
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-02-16 22:57:31 +0100

    Tweaks of WashingMachineVolume.cs

--- Assets/Scripts/WashingMachineVolume.cs
+++ Assets/Scripts/WashingMachineVolume.cs
@@ -2,89 +2,57 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class WashingMachineVolume : MonoBehaviour {
-    private float _maxVolume;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private Transform player;
-    private float _hearingDistance = .5f;
-    private Vector3 _basementDoorPosition;
-    private bool _inside;
-    private float _minVolume = .27f;
     [SerializeField] private Transform basementDoor;
+    
     [SerializeField] private float doorOpenAngle;
     [SerializeField] private float doorClosedAngle;
     [SerializeField] private float maxVolumeDoorClosed;
+    [SerializeField] private float firstFloorHeight = 0f;
 
+    private Vector3 _inverseTransformPoint;
+    private Vector3 _basementDoorPosition;
+    private Vector3 _playerPos;
+    private const float HearingDistance = .5f;
+    private float _currDoorAngle;
+    private float _openness;
+    private float _closedDoorVolume;
+    private float _maxVolume;
+    private float _distance;
+    private bool _outsideBasement;
+    private bool _inside;
+    
     private void Awake() {
-        // GetComponent<SphereCollider>().radius = hearingDistance;
         _basementDoorPosition = this.transform.position;
         _maxVolume = audioSource.volume;
     }
 
-    public float dist;
-    public float debug;
-    public float angle;
+
     private void Update() {
-        Vector3 playerPos = player.position;
-        Vector3 inverseTransformPoint = transform.InverseTransformPoint(playerPos);
+        _playerPos = player.position;
+        _inverseTransformPoint = transform.InverseTransformPoint(_playerPos);
 
-        dist = Vector3.Distance(Vector3.zero, inverseTransformPoint);
-        debug = _maxVolume * Mathf.InverseLerp(_hearingDistance, 0f, dist);
+        _distance = Vector3.Distance(Vector3.zero, _inverseTransformPoint);
 
-        bool outsideBasement = playerPos.z < _basementDoorPosition.z;
+        _outsideBasement = _playerPos.z < _basementDoorPosition.z;
 
-        float doorAngle = basementDoor.rotation.eulerAngles.y;
+        _currDoorAngle = basementDoor.rotation.eulerAngles.y;
         
-        angle = doorAngle;
+        _openness = Mathf.InverseLerp(doorClosedAngle, doorOpenAngle, _currDoorAngle);
         
-        float openness = Mathf.InverseLerp(doorClosedAngle, doorOpenAngle, doorAngle);
         
-        debug = Mathf.Lerp(maxVolumeDoorClosed, _maxVolume,  Ease.OutExpo(openness));
-        
-        float maxVolume = outsideBasement
-            ? Mathf.Lerp(maxVolumeDoorClosed, _maxVolume, openness) 
+        _closedDoorVolume = _outsideBasement
+            ? Mathf.Lerp(maxVolumeDoorClosed, _maxVolume, _openness) 
             : _maxVolume;
         
-        bool beneath = playerPos.y < -0.021;
+        bool beneath = _playerPos.y < firstFloorHeight;
         
         audioSource.volume = beneath 
             ? _maxVolume 
-            : maxVolume * Mathf.InverseLerp(_hearingDistance,0f, dist);
+            : _closedDoorVolume * Mathf.InverseLerp(HearingDistance,0f, _distance);
     }
-
-    // public float dist;
-    // private void Update() {
-    //     playerY = player.position.y;
-    //     if (PlayerIsDownstairs()) {
-    //         audioSource.volume = _maxVolume;
-    //         return;
-    //     }
-    //     
-    //     float distance = Vector3.Distance(_basementDoorPosition, player.position);
-    //     dist = distance;
-    //     
-    //     audioSource.volume = _maxVolume * Mathf.InverseLerp(hearingDistance,0f, distance);
-    // }
-// 
-    // private void OnTriggerEnter(Collider other) {
-    //     if (!other.CompareTag("Player")) return;
-    //     
-    //     _inside = true;
-    // }
-// 
-    // private void OnTriggerExit(Collider other) {
-    //     if (!other.CompareTag("Player")) return;
-// 
-    //     _inside = PlayerIsDownstairs();
-    //     Debug.Log($"exit: {!_inside}");
-    //     if (!_inside) 
-    //         audioSource.volume = _minVolume;
-    // }
-// 
-    // public float playerY;
-    // 
-    // private bool PlayerIsDownstairs() {
-    //     return player.position.y < -0.021;
-    // }
 }
