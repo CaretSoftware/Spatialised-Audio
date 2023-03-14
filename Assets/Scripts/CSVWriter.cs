167bda3e29f533b70182b24b438c8050c6eb6a6c
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-03-14 13:24:35 +0100

    Timer

--- Assets/Scripts/CSVWriter.cs
+++ Assets/Scripts/CSVWriter.cs
@@ -7,23 +7,27 @@ using UnityEngine;
 
 
 public class CSVWriter : MonoBehaviour {
-    private const string folderName = "/Data";
-    private string filename = string.Empty;
+    private const string Rubric = "Test Subject Nr, Total Elapsed Time, Shot Nr, TimeTaken, Precision Total,"+
+                                  " Precision X, Precision Y, Distance, Hit/Miss, Player Floor Shot, Ghost Floor Shot";
+    private const string FolderName = "/Data";
+    private string _filename = string.Empty;
+    public static int SubjectNumber;
+    public static int RoundNumber = 1;
+    [SerializeField] private bool writeToFile = false;
 
     private void Start() {
-        Directory.CreateDirectory(Application.dataPath + folderName); // does not create directory if exists
+        Directory.CreateDirectory(Application.dataPath + FolderName); // does not create directory if exists
         
-        int fileNumber = 0;
+        SubjectNumber = 0;
         
         do {
-            filename = Application.dataPath + $"/Data/test{(fileNumber < 10 ? ($"0{fileNumber}") : fileNumber)}.csv";
+            _filename = Application.dataPath + $"/Data/test{(SubjectNumber < 10 ? ($"0{SubjectNumber}") : SubjectNumber)}.csv";
 
-            fileNumber++;
+            SubjectNumber++;
             
-        } while(File.Exists(filename)); // don't overwrite existing files
+        } while(File.Exists(_filename)); // don't overwrite existing files
         
         CreateCSV();
-
         
         string[] data = new string[]{ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14" };
         AppendCSV(data);
@@ -34,19 +38,25 @@ public class CSVWriter : MonoBehaviour {
         data = new string[]{ "2", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14" };
         AppendCSV(data);
     }
+    
+
 
     private bool CreateCSV() {
-        if (true) { // there is data to write
-            TextWriter tw = new StreamWriter(filename, false); // append false
-            tw.WriteLine("Test Subject Nr, Total Elapsed Time, Shot Nr, TimeTaken, Precision Total, Precision X, Precision Y, Distance, Hit/Miss, Player Position Spawn, Ghost Position Spawn, Player Position Shot, Ghost Position Shot, Player Floor Shot, Ghost Floor Shot");
+        if (!writeToFile) return false;
+        
+        TextWriter tw = new StreamWriter(_filename, false); // append false
+        tw.WriteLine(Rubric);
             
-            tw.Close();
-        }
+        tw.Close();
         return true;
     }
 
     private bool AppendCSV(string[] data) {
-        TextWriter tw = new StreamWriter(filename, true); // append true
+        if (!writeToFile) return false;
+
+        RoundNumber++;
+
+        TextWriter tw = new StreamWriter(_filename, true); // append true
 
         StringBuilder sb = new StringBuilder();
 
@@ -61,4 +71,35 @@ public class CSVWriter : MonoBehaviour {
         
         return true;
     }
+}
+
+public struct ShotData {
+
+    public ShotData(int subjectNumber, float totalElapsedTime, int roundNumber, float roundElapsedTime, 
+        float precision, float precisionX, float precisionY, float distance, bool hit, int playerFloor, int ghostFloor) {
+
+        _subjectNumber = subjectNumber;
+        _totalElapsedTime = totalElapsedTime;
+        _roundNumber = roundNumber;
+        _roundElapsedTime = roundElapsedTime;
+        _precision = precision;
+        _precisionX = precisionX;
+        _precisionY = precisionY;
+        _distance = distance;
+        _hit = hit;
+        _playerFloor = playerFloor;
+        _ghostFloor = ghostFloor;
+    }
+
+    private int _subjectNumber;
+    private float _totalElapsedTime;
+    private int _roundNumber;
+    private float _roundElapsedTime;
+    private float _precision;
+    private float _precisionX;
+    private float _precisionY;
+    private float _distance;
+    private bool _hit;
+    private int _playerFloor;
+    private int _ghostFloor;
 }
